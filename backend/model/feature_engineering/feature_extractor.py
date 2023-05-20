75d31efc909d84fa9b3a31ee98f8cf582202e90c
maaja <01161548@pw.edu.pl>
2023-05-20 14:06:29 +0200

    feat: update feature extractor

--- backend/model/feature_engineering/feature_extractor.py
+++ backend/model/feature_engineering/feature_extractor.py
@@ -6,4 +6,42 @@ class FeatureExtractor:
 
     def feature_engineering(self):
         pass
+
+    def count_upper_words_in_string(self, text:str):
+        count = 0
+        for word in text:
+            if len(word) < 3:
+                continue
+            upper = 0
+            for i in range(len(word)):
+                if word[i].isupper():
+                    upper += 1
+            if upper > 3:
+                count += 1
+        return count 
     
+    def count_upper_words(self, text_col:str):
+
+        self.df[upper_count] = self.df[text_col].apply(lambda c: self.count_upper_words_in_string(c))
+
+
+
+
+    
+    def word_is_in_text(self, word, text):
+        if word in text:
+            return True
+        else:
+            return False
+        
+    def count_word(self, word, text):
+        count =  text.count(word)
+        return count
+    
+    
+
+        
+
+
+
+        
