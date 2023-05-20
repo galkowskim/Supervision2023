869d559aa671bcdbca2ba49b10083817d00ce147
hbujakow <h.bujakow@gmail.com>
2023-05-20 22:31:20 +0200

    refactor, fix bugs

--- backend/model/feature_engineering/feature_extractor.py
+++ backend/model/feature_engineering/feature_extractor.py
@@ -5,7 +5,7 @@ class FeatureExtractor:
     def __init__(self, df: pd.DataFrame) -> None:
         self.df = df.copy()
 
-    def feature_engineering(self, text_col: str, terms_dict: Dict[str, List[str]], acc_creation_date_col: pd.datetime, post_upload_date_col: pd.datetime,  postprocessed = False):
+    def feature_engineering(self, text_col: str, terms_dict: Dict[str, List[str]], acc_creation_date_col, post_upload_date_col,  postprocessed = False):
         """
         Creates features on preprocessed data or pre and postprocessed data
         """
@@ -25,7 +25,7 @@ class FeatureExtractor:
         #TODO dodac count a nie 1/0
         """
         return 1 if all([term in text for term in terms]) else 0
-    
+
     def count_word_occurrences(word_list, text):
         unique_words = set(word_list)
         common_words = unique_words.intersection(set(text.split()))
@@ -37,22 +37,22 @@ class FeatureExtractor:
         Creates new column with name new_col based on the occurence of term(s) in text column
         """
         self.df[new_col] = self.df[text_col].apply(lambda s: self._terms_in_string(terms=terms, text=s))
-    
+
     def count_number_of_uppercased_words(self, text_col: str, new_col: str = 'upper_words_count'):
         self.df[new_col] = self.df[text_col].apply(lambda s: len([el for el in s.split(' ') if el.isupper()]))
-    
+
     def count_number_of_exclamation_mark(self, text_col: str, new_col: str = "exclamation_marks_count"):
         self.df[new_col] = self.df[text_col].apply(lambda s: s.count("!"))
-    
+
     def get_n_of_days_from_account_creation(self, acc_creation_date_col: str, post_upload_date_col: str, new_col: str):
         self.df[new_col] = (self.df[acc_creation_date_col] - self.df[post_upload_date_col]).dt.components['hours']
-    
+
     def get_text_length(self, text_col: str, new_col: str = 'text_length'):
         self.df[new_col] = self.df[text_col].apply(lambda s: len(s))
-    
-    
 
-        
+
+
+
 
 
 
--- backend/model/preprocessing/preprocessor.py
+++ backend/model/preprocessing/preprocessor.py
@@ -2,6 +2,7 @@ import pandas as pd
 import re
 from nltk.tokenize import word_tokenize
 import spacy
+spacy.load('pl_core_news_sm')
 
 class Preprocessor:
     def __init__(self, df: pd.DataFrame, stopwords_file: str) -> None:
@@ -12,21 +13,23 @@ class Preprocessor:
             self.stop_words = set(content)
 
     def preprocess_data(self, text_col: str, account_creation_date_col: str, post_upload_date_col: str):
-        """ 
+        """
         Performs preprocessing tasks on text data
         """
         self.format_col_type(text_col, 'str')
         self.format_col_type(account_creation_date_col, 'datetime64[ns]')
         self.format_col_type(post_upload_date_col, 'datetime64[ns]')
-        self.remove_special_characters(text_col, chars_to_delete='[^A-Za-zążźółćśęń!]+')
+        self.remove_special_characters(
+            text_col, chars_to_delete='[^A-Za-zążźółćśęń!]+')
         self.remove_stopwords(text_col)
-    
+
     def postprocess_data(self, text_col: str):
-        """ 
+        """
         Performs postprocessing tasks on text data. These are the transformations, which would corrupt
         feature engineering process
         """
-        self.remove_special_characters(text_col, chars_to_delete='[^A-Za-zążźółćśęń]+')
+        self.remove_special_characters(
+            text_col, chars_to_delete='[^A-Za-zążźółćśęń]+')
         self.convert_text_to_lowercase(text_col)
         self.lemmatize(text_col)
         self.remove_stopwords(text_col)
@@ -36,18 +39,21 @@ class Preprocessor:
         Removes special chcarcters from text column
         """
         self.df[text_col] = self.df[text_col].str.replace('%', 'procent')
-        self.df[text_col] = self.df[text_col].apply(lambda s: re.sub(chars_to_delete, ' ', s))
+        self.df[text_col] = self.df[text_col].apply(
+            lambda s: re.sub(chars_to_delete, ' ', s))
 
     def _remove_stopwords_in_text(self, text: str):
-            word_tokens = word_tokenize(text)
-            filtered_sentence = [w for w in word_tokens if not w in self.stop_words]
-            return ' '.join(filtered_sentence)
+        word_tokens = word_tokenize(text)
+        filtered_sentence = [
+            w for w in word_tokens if not w in self.stop_words]
+        return ' '.join(filtered_sentence)
 
     def remove_stopwords(self, text_col: str):
         """
         Removes stopwords from polish language (provided we are scraping data from polish sites only)
         """
-        self.df[text_col] = self.df[text_col].apply(lambda s: self._remove_stopwords_in_text(s))
+        self.df[text_col] = self.df[text_col].apply(
+            lambda s: self._remove_stopwords_in_text(s))
 
     def _lemmatize_text(self, text):
         nlp = spacy.load("pl_core_news_sm")
@@ -58,7 +64,8 @@ class Preprocessor:
         """
         Applies lemmatization process to text column, i.e. extract semantically important information about the word from every word in string.
         """
-        self.df[text_col] = self.df[text_col].apply(lambda s: self._lemmatize_text(s))
+        self.df[text_col] = self.df[text_col].apply(
+            lambda s: self._lemmatize_text(s))
 
     def convert_text_to_lowercase(self, text_col: str):
         """
@@ -66,12 +73,9 @@ class Preprocessor:
         """
 
         self.df[text_col] = self.df[text_col].apply(lambda s: s.lower())
-    
+
     def format_col_type(self, col: str, target_type: str):
         """
         Converts column to the desired type
         """
         self.df[col] = self.df[col].astype(target_type)
-        
-
-
