fcf722bf0c195ccd44cb6f4b2eb8f37511985421
WJakubowsk <72559388+WJakubowsk@users.noreply.github.com>
2023-05-20 20:23:01 +0200

    add text length feature

--- backend/model/feature_engineering/feature_extractor.py
+++ backend/model/feature_engineering/feature_extractor.py
@@ -3,15 +3,15 @@ from typing import List, Dict
 
 class FeatureExtractor:
     def __init__(self, df: pd.DataFrame) -> None:
-        self.df = df
+        self.df = df.copy()
 
-    def feature_engineering(self, text_col: str, terms_dict: Dict[List[str]], postprocessed = False):
+    def feature_engineering(self, text_col: str, terms_dict: Dict[str, List[str]], postprocessed = False):
         """
         Creates features on preprocessed data or pre and postprocessed data
         """
         if not postprocessed:
-            self.count_number_of_exclamation_mark()
-            self.count_number_of_uppercased_words()
+            self.count_number_of_exclamation_mark(text_col)
+            self.count_number_of_uppercased_words(text_col)
         else:
             for col_name, terms in terms_dict.items():
                 self.extract_term_occurence(text_col, terms=terms, new_col=col_name)
@@ -20,8 +20,15 @@ class FeatureExtractor:
         """
         Returns 1 if all of the terms specified in list are present in the text.
         Helper function for extract_term_occurence function
+        #TODO dodac count a nie 1/0
         """
         return 1 if all([term in text for term in terms]) else 0
+    
+    def count_word_occurrences(word_list, text):
+        unique_words = set(word_list)
+        common_words = unique_words.intersection(set(text.split()))
+        total_count = sum(text.split().count(word) for word in common_words)
+        return total_count
 
     def extract_term_occurence(self, text_col: str, terms: List[str], new_col: str):
         """
@@ -35,8 +42,11 @@ class FeatureExtractor:
     def count_number_of_exclamation_mark(self, text_col: str, new_col: str = "exclamation_marks_count"):
         self.df[new_col] = self.df[text_col].apply(lambda s: s.count("!"))
     
-    def get_n_of_days_from_account_creation(self, acc_creation_date_col: str, post_upload_date_col: str):
-        return self.df[acc_creation_date_col] - self.df[post_upload_date_col]
+    def get_n_of_days_from_account_creation(self, acc_creation_date_col: str, post_upload_date_col: str, new_col: str):
+        self.df[new_col] = (self.df[acc_creation_date_col] - self.df[post_upload_date_col]).dt.components['hours']
+    
+    def get_text_length(self, text_col: str, new_col: str = 'text_length'):
+        self.df[new_col] = self.df[text_col].apply(lambda s: len(s))
     
     
 
