bb4400021b6790f72d678bc2d0e7881d69856a0b
hbujakow <72279632+hbujakow@users.noreply.github.com>
2023-05-21 09:52:55 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 source venv/bin/activate
 
-cd /home/galkowskim/Desktop/Supervision2023/backend
+cd backend
 
 python manage.py shell -c "from supervision_app.tasks import scrape_data; scrape_data()"
 
